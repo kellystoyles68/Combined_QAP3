9c7941b22b36b109c7cf3eea2147539fdd6c7bad
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-07-14 17:15:05 -0230

    committing sql statements

--- sql/database.sql
+++
@@ -1,8 +0,0 @@
-create table books (
-    ISBN BIGINT primary key,
-    title varchar (255) not null,
-    author varchar (100) not null,
-    genre varchar ot null,
-    year_published int not null
-)
-
